aec7137b0829bc443ece5bc8e08a08edad02b4bd
Erwin James Will <jwill.will@gmail.com>
2013-07-18 02:57:58 -0400

    added featured content
    will pull data from website.  I want it to be a collapsible title
    featured then the title of the conent and once the collapsible is
    clicked the details are shown but i can't get the title and details to
    show up.  Also right now the title is showing up twice for some reason.

---
+++ www/featured.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+var output = $('#featured');
+
+	$.ajax({
+		type: 'GET',
+		url: 'http://www.jmuafterdark.com/featured.php',
+		dataType: 'jsonp',
+		jsonp: 'jsoncallback',
+		timeout: 5000,
+		success: function(data, status){
+			$.each(data, function(i,item){ 
+                   var featured ='<span>'+item.title+'<span>';
+
+				output.append(featured);
+			});
+		},
+		error: function(){
+		   output.text('There was an error loading the data.');
+		}
+	});
+                  
+var output = $('#feature');
+
+        $.ajax({
+            type: 'GET',
+            url: 'http://www.jmuafterdark.com/featured.php',
+            dataType: 'jsonp',
+            jsonp: 'jsoncallback',
+            timeout: 5000,
+            success: function(data, status){
+            $.each(data, function(i,item){
+                var featured ='<br>'+item.detail+'</br>';
+                                
+                output.append(feature);
+            });
+        },
+        error: function(){
+            output.text('There was an error loading the data.');
+        } 
+    });
+
+});
