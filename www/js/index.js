edf2f0b1fd89eac6782162f5b0291d9cf5f8787a
Erwin James Will <jwill.will@gmail.com>
2013-07-27 01:10:33 -0400

    pushwoosh
    think i got pushwoosh to work.

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,32 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+function initPushwoosh() {
+    var pushNotification = window.plugins.pushNotification;
+    pushNotification.onDeviceReady();
+    
+    pushNotification.registerDevice({alert:true, badge:true, sound:true, pw_appid:"4F8A0-B1178", appname:"JMU AfterDark"},
+                                    function(status) {
+                                    var deviceToken = status['deviceToken'];
+                                    console.warn('registerDevice: ' + deviceToken);
+                                    },
+                                    function(status) {
+                                    console.warn('failed to register : ' + JSON.stringify(status));
+                                    navigator.notification.alert(JSON.stringify(['failed to register ', status]));
+                                    }
+                                    );
+    
+    pushNotification.setApplicationIconBadgeNumber(0);
+    
+    document.addEventListener('push-notification', function(event) {
+                                                            var notification = event.notification;
+                                                        navigator.notification.alert(notification.aps.alert);
+                                                                pushNotification.setApplicationIconBadgeNumber(0);
+                                                        });
+}
+
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -31,8 +57,12 @@ var app = {
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
+    // function, we must explicity' call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+        // note that this is an event handler so the scope is that of the event
+        // so we need to call app.report(), and not this.report()
+        initPushwoosh();
+        
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
